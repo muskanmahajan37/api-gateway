8e0417e6e61378217ce5bb39adfba7efde503f6a
vjanz <valon.januzaj98@gmail.com>
2020-05-25 16:35:04 +0200

    RouteBindingServiceProvider.php for route model binding

---
+++ app/Providers/RouteBindingServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Providers;
+use mmghv\LumenRouteBinding\RouteBindingServiceProvider as BaseServiceProvider;
+
+
+class RouteBindingServiceProvider extends BaseServiceProvider
+{
+    public function boot()
+    {
+        $binder = $this->binder;
+        $binder->bind('user', 'App\User');
+    }
+}
