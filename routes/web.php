67c91ec42594e8649edc10f6f126bfb0c23b84e4
vjanz <valon.januzaj98@gmail.com>
2020-05-31 21:32:57 +0200

    Formated and added route for user profile

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,47 @@
-
 <?php
 
-#public routes
-$router->post('/login','AuthController@authenticate');
-$router->post('/register','AuthController@register');
-$router->get('/services', 'Service\ServiceController@index');
-$router->get('/categories/{category}/services', 'Service\ServiceController@findByCategory');
+// Auth routes
+$router->post('/login', 'AuthController@authenticate');
+$router->post('/register', 'AuthController@register');
 
+// User
+$router->get('users/{user}/services', 'Service\ServiceController@findByUser');
+$router->get('/profile/{username}', 'UserController@getUserByUsername');
+
+// Node micro test
 $router->get("/charge", 'Payment\PaymentController@index');
 
-$router->get('/categories','Service\CategoryController@index');
-$router->get('/categories/{category}','Service\CategoryController@show');
+// Services
+$router->get('/services', 'Service\ServiceController@index');
+
+// Categories - Subcategories
+$router->get('/categories/{category}/services', 'Service\ServiceController@findByCategory');
+$router->get('/categories', 'Service\CategoryController@index');
+$router->get('/categories/{category}', 'Service\CategoryController@show');
 $router->get('/categories/{category}/subcategories', 'Service\SubCategoryController@findByCategory');
-$router->get('/subcategories','Service\SubCategoryController@index');
+$router->get('/subcategories', 'Service\SubCategoryController@index');
 $router->get('subcategories/{subcategory}/services', 'Service\ServiceController@findBySubCategory');
-$router->get('users/{user}/services', 'Service\ServiceController@findByUser');
 
-$router->post('services', 'Service\ServiceController@store');
 
-$router->group(['middleware' => 'jwt.auth'], function() use ($router){
+// Protected Routes
+$router->group(['middleware' => 'jwt.auth'], function () use ($router) {
     # users
-    $router->get('/users','UserController@index');
-    $router->get('/users/{user}','UserController@show');
-    $router->delete('/users/{user}','UserController@destroy');
-    $router->patch('/users/{user}','UserController@update');
+    $router->get('/users', 'UserController@index');
+    $router->get('/users/{user}', 'UserController@show');
+    $router->delete('/users/{user}', 'UserController@destroy');
+    $router->patch('/users/{user}', 'UserController@update');
 
-
-    $router->get('services/{service}','Service\ServiceController@show');
-    $router->patch('services/{service}','Service\ServiceController@update');
-    $router->delete('services/{service}','Service\ServiceController@destroy');
+    $router->get('services/{service}', 'Service\ServiceController@show');
+    $router->patch('services/{service}', 'Service\ServiceController@update');
+    $router->delete('services/{service}', 'Service\ServiceController@destroy');
+    $router->post('services', 'Service\ServiceController@store');
 
     $router->post('subcategories', 'Service\SubCategoryController@store');
     $router->get('subcategories/{subcategory}', 'Service\SubCategoryController@show');
     $router->put('subcategories/{subcategory}', 'Service\SubCategoryController@update');
 
     $router->post('categories', 'Service\CategoryController@store');
-    $router->put('categories/{category}','Service\CategoryController@update');
+    $router->put('categories/{category}', 'Service\CategoryController@update');
 
     $router->get('/authors', 'Author\AuthorController@index');
     $router->post('/authors', 'Author\AuthorController@store');
@@ -43,9 +49,6 @@ $router->group(['middleware' => 'jwt.auth'], function() use ($router){
     $router->put('/authors/{author}', 'Author\AuthorController@update');
     $router->patch('/authors/{author}', 'Author\AuthorController@update');
     $router->delete('/authors/{author}', 'Author\AuthorController@destroy');
-
-
-
 });
 
 
