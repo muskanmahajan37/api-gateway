6ae0790c75dd2f139008d744420a5e062c02d658
vjanz <valon.januzaj98@gmail.com>
2020-06-15 23:57:02 +0200

    Service configuration for github login

--- config/services.php
+++ config/services.php
@@ -6,15 +6,21 @@ return [
         'secret'  =>  env('AUTHORS_SERVICE_SECRET'),
     ],
 
-    'service'   =>  [
-        'base_uri'  =>  env('SERVICE_SERVICE_BASE_URL'),
-        'secret'  =>  env('SERVICE_SERVICE_SECRET'),
+    'service' => [
+        'base_uri' => env('SERVICE_SERVICE_BASE_URL'),
+        'secret' => env('SERVICE_SERVICE_SECRET'),
     ],
 
-    'payment'   =>  [
-        'base_uri'  =>  env('PAYMENT_SERVICE_BASE_URL'),
-        'secret'  =>  env('PAYMENT_SERVICE_SECRET'),
+    'payment' => [
+        'base_uri' => env('PAYMENT_SERVICE_BASE_URL'),
+        'secret' => env('PAYMENT_SERVICE_SECRET'),
     ],
+
+    'github' => [
+        'client_id' => env("GITHUB_CLIENT_ID"),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env("GITHUB_REDIRECT_URL")
+    ]
 
 
 ];
