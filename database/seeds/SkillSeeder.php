beeb23ff8e59b1bcd92c732668a95398b3b65a03
Taulant Dema <taulantdema12@gmail.com>
2020-06-19 19:17:07 +0200

    added seed for Skill

---
+++ database/seeds/SkillSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Skill;
+
+class SkillSeeder extends Seeder
+{
+    public function run()
+    {
+        Skill::create(['name' => 'Java', 'user_id' =>2]);
+        Skill::create(['name' => 'Html', 'user_id' =>2]);
+        Skill::create(['name' => 'Laravel', 'user_id' =>2]);
+    }
+}
