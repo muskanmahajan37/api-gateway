196d5fcc27b42df0c0f1ed1d53accd3dd37a932e
Taulant Dema <taulantdema12@gmail.com>
2020-07-04 23:55:59 +0200

    added PasswordResetSuccess

---
+++ app/Notifications/PasswordResetSuccess.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Notifications;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+class PasswordResetSuccess extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('You are changed your password successful.')
+            ->line('If you did change password, no further action is required.')
+            ->line('If you did not change password, protect your account.');
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
