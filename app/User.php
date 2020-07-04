dd6ea3e9b97c450113cf4f48830af867347b48b6
Taulant Dema <taulantdema12@gmail.com>
2020-07-04 23:56:40 +0200

    changes to User model

--- app/User.php
+++ app/User.php
@@ -3,21 +3,23 @@
 namespace App;
 
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Notifications\Notifiable as Notifiable;
 use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable;
-
+    use Authenticatable, Authorizable, Notifiable;
+    protected $dates = ['deleted_at'];
     protected $fillable = [
-        'name', 'email','password', 'image'
+        'name', 'email','password', 'image','active', 'activation_token'
     ];
 
     protected $hidden = [
-        'password','email_verified_at','remember_token','created_at','updated_at'
+        'password','email_verified_at','remember_token','created_at','updated_at','activation_token'
     ];
     public function role(){
         return $this->belongsTo(Role::class);
