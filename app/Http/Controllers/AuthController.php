81c959cfe976ebaa7a09a1b4d4a317af02862c86
Taulant Dema <taulantdema12@gmail.com>
2020-07-04 23:54:33 +0200

    changes to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,10 +7,12 @@ use App\Role;
 use App\User;
 use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Validator;
+use App\Notifications\SignupActivate;
 
 class AuthController extends BaseController
 {
@@ -40,12 +42,14 @@ class AuthController extends BaseController
             'email' => 'required|email',
             'password' => 'required'
         ]);
-
-
         $user = User::where('email', $this->request->input('email'))->first();
         if (!$user) {
             return response()->json([
                 'message' => 'User does not exist.'
+            ], 400);
+        }else if ($user->active===0) { //qitu e bon !=1(to be done)
+            return response()->json([
+                'message' => 'Please confirm your account'
             ], 400);
         }
         $userResponse = new User();
@@ -54,7 +58,6 @@ class AuthController extends BaseController
         $userResponse->name = $user->name;
         $userResponse->image = $user->image;
         $userResponse->username = $user->username;
-
         if (Hash::check($this->request->input('password'), $user->password)) {
             return response()->json([
                 'user' => $userResponse,
@@ -76,6 +79,7 @@ class AuthController extends BaseController
             'password' => 'required|confirmed',
             'image' => 'image'
         ];
+
         $fields = $this->validate($request, $rules);
         $fields['password'] = Hash::make($request->password);
         if ($request->hasFile('image')) {
@@ -85,8 +89,10 @@ class AuthController extends BaseController
         } else {
             $fileToStore = 'noimage.jpg';
         }
+        $activation_token= str_random(60);
         $user = new User($fields);
         $user['image']=$fileToStore;
+        $user['activation_token']=$activation_token;
         $role = Role::find(1);
         $user->username = $request['username'];
         $user->save();
@@ -97,12 +103,29 @@ class AuthController extends BaseController
         $userResponse->name = $user->name;
         $userResponse->image = $fileToStore;
         $userResponse->username = $user->username;
+        $userResponse->activation_token=$user->activation_token;
+        $user->notify(new SignupActivate($userResponse));
+
+        //ketu val duhet me bo masi ta bon activate me i ardh ai emaili welcome to flex ose mrenda ni emaili
         Mail::to($user->email)->send(new Welcome($fields));
         return response()->json([
             'created' => true,
             'access_token' => $this->jwt($user),
             'user' => $userResponse
         ], 201);
+    }
+    public function signupActivate($token)
+    {
+        $user = User::where('activation_token', $token)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'This activation token is invalid.'
+            ], 404);
+        }
+        $user->active = true;
+        $user->activation_token = '';
+        $user->save();
+        return $user;
     }
 
 }
