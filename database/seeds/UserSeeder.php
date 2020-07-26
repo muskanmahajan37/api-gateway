7f0d93f52460e209a103a8929805212a8075688e
vjanz <valon.januzaj98@gmail.com>
2020-07-26 21:00:06 +0200

    Added seeders for presentation

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,22 +13,17 @@ class UserSeeder extends Seeder
         $password = env("FIRST_ADMIN_PASSWORD");
         $email = env("FIRST_ADMIN_EMAIL");
         $username = env("FIRST_ADMIN_USERNAME");
-
         $userPassword = env("FIRST_USER_PASSWORD");
         $userEmail = env("FIRST_USER_EMAIL");
         $userUsername = env("FIRST_USER_USERNAME");
-
         $password_hashed = Hash::make($password);
         $user_password_hashed = Hash::make($userPassword);
-
         $userRole = Role::find(1);
         $adminRole = Role::find(2);
-
-        $admin = User::create(['name' => 'admin', 'username' => $username, 'email' => $email, 'password' => $password_hashed, 'image' => 'noimage.jpg','active'=>1,'activation_token'=>'']);
         $user = User::create(['name' => 'user', 'username' => $userUsername, 'email' => $userEmail, 'password' => $user_password_hashed, 'image' => 'noimage.jpg','active'=>1,'activation_token'=>'']);
-
-        $userRole->users()->save($user);
+        $admin = User::create(['name' => 'admin', 'username' => $username, 'email' => $email, 'password' => $password_hashed, 'image' => 'noimage.jpg','active'=>1,'activation_token'=>'']);
         $adminRole->users()->save($admin);
+        $userRole->users()->save($user);
     }
 
 
